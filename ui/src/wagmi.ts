52261fe4952913906cce921dd87e8f8355e8d4af
sripwoud <me@sripwoud.xyz>
2024-07-24 17:25:45 +0200

    chore: scaffold ui

---
+++ ui/src/wagmi.ts
@@ -0,0 +1,31 @@
+import { cookieStorage, createConfig, createStorage, http } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
+import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
+
+export function getConfig() {
+  const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID
+  if (!projectId) throw new Error('NEXT_PUBLIC_WC_PROJECT_ID is not set')
+
+  return createConfig({
+    chains: [mainnet, sepolia],
+    connectors: [
+      injected(),
+      coinbaseWallet(),
+      walletConnect({ projectId }),
+    ],
+    storage: createStorage({
+      storage: cookieStorage,
+    }),
+    ssr: true,
+    transports: {
+      [mainnet.id]: http(),
+      [sepolia.id]: http(),
+    },
+  })
+}
+
+declare module 'wagmi' {
+  interface Register {
+    config: ReturnType<typeof getConfig>
+  }
+}
